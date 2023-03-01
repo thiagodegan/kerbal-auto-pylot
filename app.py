074b8ed436ec46f9a2d2cb780616065688a498b5
thiagodegan <thiago_degan@gsretail.com.br>
2023-03-01 20:38:27 -0300

    feat: reenderiza template html #1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
 
 
-@app.route("/")
-def hello():
-    return "Hello world!!!"
+@app.route('/')
+def index():
+    return render_template('index.html')
