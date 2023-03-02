bd2cbf03d7c9fc5b2584e5488b658fa725671f44
thiagodegan <thiago_degan@gsretail.com.br>
2023-03-01 22:20:09 -0300

    Ativar e conectar a biblioteca KRPC ao KSP #2

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 from flask import Flask, render_template
+import krpc
+
+
+conn = None
+vessel = None
+
+
+def conectarKsp():
+    """Conecta no KSP"""
+    try:
+        return krpc.connect(name='Kerbal Auto-Pylot')
+    except ConnectionRefusedError:
+        print('Conexão recusada, verifique se o KRPC está ativo...')
+
+
+def getVessel(conn):
+    """Retorna a nave"""
+    if (conn != None):
+        return conn.space_center.active_vessel
+    else:
+        print('A conexão não foi estabelicida!')
+    return None
+
+
+conn = conectarKsp()
+vessel = getVessel(conn=conn)
 
 app = Flask(__name__)
 
@@ -7,4 +33,7 @@ app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if vessel == None:
+        return render_template('index.html', conectado=False)
+    else:
+        return render_template('index.html', conectado=True, nave=getVessel, conn=conn)
